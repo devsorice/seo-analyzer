40df073d98cb7eb9d0c9021b8fda68825d330d92
Francesco <devsorice@gmail.com>
2025-04-06 22:04:44 +0900

    save report to file

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,8 @@
 import { services } from './init';
 import * as readline from "readline";
+import * as fs from 'fs';
+import * as path from 'path';
+import { v7 } from 'uuid';
 
 async function main() {
   const url = process.argv[2];
@@ -34,6 +37,19 @@ async function main() {
         console.log(`Will analyze page with lighthouse ${pageInfo.url}`);
         const lighthouseResult = await services.lighthouse.audit(pageInfo.url);
         console.log(lighthouseResult);
+
+        //we will save the full result in a local file
+        const dir = path.join(__dirname, '..', 'lighthouse-results');
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        
+        const fullPageAnalysis = {
+          pageInfo: pageInfo.toDictionary(),
+          lighthouseResult:lighthouseResult
+        }
+        
+        fs.writeFileSync(`${dir}/${v7()}.json`, JSON.stringify(fullPageAnalysis, null, 2));
 
         //We will ask the user if we should continue crawling fron the cli
         const answer = await shouldContinue();
